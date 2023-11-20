47b5510613ebd0f699fae33a4bfc5c915ff9daff
Platonov A.V <74860073+antony-vipplatonov@users.noreply.github.com>
2023-11-20 10:54:48 +0300

    First lab

--- bmstu_lab/models.py
+++ bmstu_lab/models.py
@@ -8,11 +8,12 @@ class Compaund(models.Model):
     country = models.CharField(max_length=50, blank=True, null=True)
     victory = models.IntegerField()
     status = models.CharField(max_length=1)
-    creatorname = models.CharField(max_length=50)
-    moderatorname = models.CharField(max_length=50)
-    datacreate = models.DateField()
-    dataform = models.DateField()
-    dataend = models.DateField()
+    creatorname = models.ForeignKey('Users', models.DO_NOTHING, db_column='creatorname')
+    moderatorname = models.ForeignKey('Users', models.DO_NOTHING, db_column='moderatorname', related_name='compaund_moderatorname_set')
+    datacreate = models.DateTimeField()
+    dataform = models.DateTimeField()
+    dataend = models.DateTimeField()
+    battledate = models.DateField()
 
     class Meta:
         managed = False
@@ -20,8 +21,9 @@ class Compaund(models.Model):
 
 
 class CompaundShips(models.Model):
-    idship = models.OneToOneField('Ship', models.DO_NOTHING, db_column='idship', primary_key=True)
-    idcompaund = models.ForeignKey(Compaund, models.DO_NOTHING, db_column='idcompaund')
+    id = models.IntegerField(primary_key=True)
+    idship = models.ForeignKey('Ship', models.DO_NOTHING, db_column='idship', blank=True, null=True)
+    idcompaund = models.ForeignKey(Compaund, models.DO_NOTHING, db_column='idcompaund', blank=True, null=True)
     captain = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
@@ -40,7 +42,6 @@ class Ship(models.Model):
     length = models.FloatField()
     speed = models.FloatField()
     status = models.CharField(max_length=15)
-    image = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
@@ -53,8 +54,121 @@ class Users(models.Model):
     password = models.CharField(max_length=20, blank=True, null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
     phone = models.CharField(max_length=13, blank=True, null=True)
+    ismoderator = models.BooleanField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'Users'
 
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
