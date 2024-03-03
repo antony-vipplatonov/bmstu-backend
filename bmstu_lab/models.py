e7af1b5cb1b0a42d2031355cedf776ad1f728a86
Platonov A.V <74860073+antony-vipplatonov@users.noreply.github.com>
2024-03-03 23:49:54 +0300

    Commit

--- bmstu_lab/models.py
+++ bmstu_lab/models.py
@@ -1,36 +1,36 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import UserManager, PermissionsMixin
 from django.db import models
 
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.TextField(blank=True, null=True)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=13, blank=True, null=True)
+    is_staff = models.BooleanField(null=True)
+
+    class Meta:
+        db_table = 'Users'
 
 class Compaund(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
-    admiralname = models.CharField(max_length=50)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    admiralname = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=50, blank=True, null=True)
-    victory = models.IntegerField()
-    status = models.CharField(max_length=1)
-    creatorname = models.ForeignKey('Users', models.DO_NOTHING, db_column='creatorname')
-    moderatorname = models.ForeignKey('Users', models.DO_NOTHING, db_column='moderatorname', related_name='compaund_moderatorname_set')
-    datacreate = models.DateTimeField()
-    dataform = models.DateTimeField()
-    dataend = models.DateTimeField()
-    battledate = models.DateField()
+    victory = models.IntegerField(default=0)
+    status = models.CharField(default="черновик")
+    creatorname = models.ForeignKey(Users, models.DO_NOTHING, db_column='creatorname')
+    moderatorname = models.ForeignKey(Users, models.DO_NOTHING, db_column='moderatorname', related_name='compaund_moderatorname_set', null=True)
+    datacreate = models.DateTimeField(auto_now=True)
+    dataform = models.DateTimeField(blank=True, null=True)
+    dataend = models.DateTimeField(blank=True, null=True)
+    battledate = models.DateField(blank=True, null=True)
+
 
     class Meta:
         managed = False
         db_table = 'Compaund'
-
-
-class CompaundShips(models.Model):
-    id = models.IntegerField(primary_key=True)
-    idship = models.ForeignKey('Ship', models.DO_NOTHING, db_column='idship', blank=True, null=True)
-    idcompaund = models.ForeignKey(Compaund, models.DO_NOTHING, db_column='idcompaund', blank=True, null=True)
-    captain = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'Compaund ships'
-        unique_together = (('idship', 'idcompaund'),)
-
 
 class Ship(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -41,135 +41,20 @@ class Ship(models.Model):
     displacement = models.FloatField()
     length = models.FloatField()
     speed = models.FloatField()
-    status = models.CharField(max_length=15)
-    image_src=models.TextField()
+    status = models.CharField(max_length=15, default="действует")
+    image_src = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'Ship'
 
 
-class Users(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=20, blank=True, null=True)
-    email = models.CharField(max_length=50, blank=True, null=True)
-    phone = models.CharField(max_length=13, blank=True, null=True)
-    ismoderator = models.BooleanField(blank=True, null=True)
+class CompaundShips(models.Model):
+    id = models.AutoField(primary_key=True)
+    idship = models.ForeignKey(Ship, models.DO_NOTHING, db_column='idship', blank=True, null=True)
+    idcompaund = models.ForeignKey(Compaund, models.DO_NOTHING, db_column='idcompaund', blank=True, null=True)
+    captain = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
-        managed = False
-        db_table = 'Users'
-
-
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
+        db_table = 'Compaund ships'
+        unique_together = (('idship', 'idcompaund'),)
--- bmstu_lab/views.py
+++ bmstu_lab/views.py
@@ -5,40 +5,21 @@ from datetime import date
 from django.shortcuts import render
 from django.conf.urls.static import static
 import psycopg2 as ps
-from .models import Ship
+from .models import *
 conn = ps.connect(dbname="seabattles", host="localhost", user="student", password="pass", port="5432")
 cursor = conn.cursor()
 
-'''
-allItems = [
-            {'name': "Авианосец Дзуйкаку",
-             'src': '/static/zuikaku.jpg', 'id': 1,
-             'desc': 'Зенитное вооружение корабля: 16 x 127-мм, бронирование: до 21,5 см',
-             'params': [{'year': 'Год ввода в строй - 1941', 'displacement': 'Водоизмещение 30 000 т', 'length': 'Длина корпуса - 237 м', 'speed': 'Скорость хода 34 узла'}],
-            },
-            {'name': "Линкор Пенсильвания",
-             'src': '/static/Pensilvania.jpg', 'id': 2,
-             'desc': 'Вооружение корабля:  	4 × 3 — 356-мм и 22 × 1 — 127-мм, бронирование: 343 мм',
-             'params': [{'year': 'Год ввода в строй - 1916', 'displacement': 'Водоизмещение 31 400 т', 'length': 'Длина корпуса - 185,4 м', 'speed': 'Скорость хода 21 узла'}]},
-            {'name': "Тяжёлый крейсер Нати",
-             'src': '/static/Nachi.jpg', 'id': 3,
-             'desc': 'Вооружение корабля: 5 × 2 — 200-мм, бронирование: 102 мм',
-             'params': [{'year': 'Год ввода в строй - 1928', 'displacement': 'Водоизмещение 15 933 т', 'length': 'Длина корпуса - 203,76 м', 'speed': 'Скорость хода 35,5 узла'}]},
-        ]
-'''
+draftItems = [1,2,3]
 
 def shipList(request, sear = ""):
     return render(request, 'shipList.html', {'data': {'shipList': Ship.objects.filter(status = "действует").filter(name__icontains=sear).order_by('year'),
-        'src' : sear
-    }})
+        'src' : sear,
+        'draftCompaund': len(Ship.objects.filter(id__in = [i['idship_id'] for i in CompaundShips.objects.filter(idcompaund = 56).values()]).order_by('year'))
+        }
+    })
 
 
 def search(request):
-    '''try:
-        searchQuery = request.GET['text']
-    except:
-        searchQuery = ''
-    return shipList(request, searchQuery)'''
     delId = request.POST.get("del", -1)
     searchQuery = ''
     if delId == -1:
@@ -51,4 +32,7 @@ def search(request):
 
 def getShip(request, id):
     return render(request, 'ship.html', {'data':Ship.objects.filter(id = id)[0]})
+
+def getDraft(request):
+    return render(request, 'draftCompaund.html', {'data':Ship.objects.filter(id__in = [i['idship_id'] for i in CompaundShips.objects.filter(idcompaund = 56).values()]).order_by('year')})
 
